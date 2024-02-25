eaa866b549baa73e01dd04b107525d07d19f9ef4
Asp1kkk <a5p1k@yandex.ru>
2024-02-25 16:26:10 +0300

    debugged operations and added mul and rmul

--- class3/homework.py
+++ class3/homework.py
@@ -3,46 +3,60 @@ from typing import Union
 
 # 1 and 2
 
-class Fracton:
+class Fraction:
 	def __init__(self, a:int = 0, b:int = 0) -> None:
 		self.a = a
 		self.b = b
 
-	def __eq__(self, other: Union[int, float, 'Fracton']) -> bool:
+	def __eq__(self, other: Union[int, float, 'Fraction']) -> bool:
 		if isinstance(other, int | float):
-			return self.a / self.b == other
+			return self.a / (self.b if self.b != 0 else 1) == other
 		else:
-			return self.a / self.b == other.a / other.b
+			return self.a / (self.b if self.b != 0 else 1) == other.a / (other.b if other.b != 0 else 1)
 		
-	def __ne__(self, other: Union[int, float, 'Fracton']) -> bool:
+	def __ne__(self, other: Union[int, float, 'Fraction']) -> bool:
 		if isinstance(other, int | float):
-			return self.a / self.b != other
+			return self.a / (self.b if self.b != 0 else 1) != other
 		else:
-			return self.a / self.b != other.a / other.b
+			return self.a / (self.b if self.b != 0 else 1) != other.a / (other.b if other.b != 0 else 1)
 		
-	def __gt__(self, other: Union[int, float, 'Fracton']) -> bool:
+	def __gt__(self, other: Union[int, float, 'Fraction']) -> bool:
 		if isinstance(other, int | float):
-			return self.a / self.b > other
+			return self.a / (self.b if self.b != 0 else 1) > other
 		else:
-			return self.a / self.b > other.a / other.b
+			return self.a / (self.b if self.b != 0 else 1) > other.a / (other.b if other.b != 0 else 1)
 
-	def __lt__(self, other: Union[int, float, 'Fracton']) -> bool:
+	def __lt__(self, other: Union[int, float, 'Fraction']) -> bool:
 		if isinstance(other, int | float):
-			return self.a / self.b < other
+			return self.a / (self.b if self.b != 0 else 1) < other
 		else:
-			return self.a / self.b < other.a / other.b
+			return self.a / (self.b if self.b != 0 else 1) < other.a / (other.b if other.b != 0 else 1)
 
-	def __ge__(self, other: Union[int, float, 'Fracton']) -> bool:
+	def __ge__(self, other: Union[int, float, 'Fraction']) -> bool:
 		if isinstance(other, int | float):
-			return self.a / self.b >= other
+			return self.a / (self.b if self.b != 0 else 1) >= other
 		else:
-			return self.a / self.b >= other.a / other.b
+			return self.a / (self.b if self.b != 0 else 1) >= other.a / (other.b if other.b != 0 else 1)
 
-	def __le__(self, other: Union[int, float, 'Fracton']) -> bool:
+	def __le__(self, other: Union[int, float, 'Fraction']) -> bool:
 		if isinstance(other, int | float):
-			return self.a / self.b <= other
+			return self.a / (self.b if self.b != 0 else 1) <= other
 		else:
-			return self.a / self.b <= other.a / other.b
+			return self.a / (self.b if self.b != 0 else 1) <= other.a / (other.b if other.b != 0 else 1)
+
+	def __mul__(self, other) -> 'Fraction':
+		if isinstance(other, Fraction):
+			return Fraction(self.a * other.a, self.b * (other.b if other.b != 0 else 1)).__reduce()
+		
+		if isinstance(other, int):
+			return Fraction(self.a * other, self.b if self.b != 0 else 1).__reduce()
+		
+		if isinstance(other, float):
+			return 
+
+		
+	def __rmul__(self, other) -> 'Fraction':
+		return self.__mul__(other)
 
 	def __reduce(self):
 		gcd = math.gcd(self.a, self.b)
@@ -63,19 +77,19 @@ class Fracton:
 			nums = [int(num) for num in string.split()]
 		
 		if len(nums) == 1:
-			return Fracton(nums[0])
+			return Fraction(nums[0])
 		elif len(nums) == 2:
-			return Fracton(nums[0], nums[1])
+			return Fraction(nums[0], nums[1])
 
 
 	def __str__(self) -> str:
 		if self.b == 0 and self.a == 0:
 			return "0"
-		if type(self.a) == str:
+		if isinstance(self.a, str):
 			return	self.__convert(self.a).__reduce()
 		if self.b == 0 and self.a != 0:
 			return f"{self.a}"
 		return self.__reduce()
 		
 
-print(Fracton(-5, 10))
+print(Fraction(10) == 10)
