69d0f9f7dd17ab6c66a19397df94a52dba1bc31f
Asp1kkk <a5p1k@yandex.ru>
2024-02-29 21:07:02 +0300

    Homework done: test play method completed

--- class4__tests/homework/test/test_play_method.py
+++ class4__tests/homework/test/test_play_method.py
@@ -4,3 +4,10 @@ from app.flashcards import FlashCards
 class Test_delete(unittest.TestCase):
 	def setUp(self) -> None:
 		self.flash = FlashCards()
+	
+	def test_Empty(self):
+		self.flash.words = []
+		self.assertEqual(self.flash.play(), "В словаре нет слов")
+
+if __name__ == "__main__":
+	unittest.main()
