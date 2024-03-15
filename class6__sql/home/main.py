197284269c5f9eea5effd60ade294ea733ffe27e
Asp1kkk <a5p1k@yandex.ru>
2024-03-15 15:05:46 +0300

    comedyQuery

--- class6__sql/home/main.py
+++ class6__sql/home/main.py
@@ -18,3 +18,17 @@ ON rating.title_id == titles.title_id
 ORDER BY rating DESC
 LIMIT 50
 """
+
+comedyQuery = """
+SELECt title, premiered, rating, votes, genre_name
+FROM titles
+JOIN rating
+ON rating.title_id == titles.title_id
+JOIN film_genres
+ON film_genres.title_id == titles.title_id
+JOIN genre_types
+ON genre_types.id == film_genres.genre_id
+WHERE premiered == 2019 AND genre_name == "Comedy" AND rating >= 7
+ORDER BY VOTES DESC
+LIMIT 10
+"""
